ca53923c8cb9f2640b331550ec0272c268c32dca
julian Henao <julianhm9612@gmail.com>
2020-04-23 22:29:06 -0500

    Add details and list of heroes

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -3,23 +3,28 @@ import { Component, OnInit } from '@angular/core';
 // Interfaces
 import { Hero } from '../hero';
 
+// Mocks
+import { HEROES } from '../mock-heroes';
+
 @Component({
   selector: 'app-heroes',
   templateUrl: './heroes.component.html',
   styleUrls: ['./heroes.component.css']
 })
 export class HeroesComponent implements OnInit {
-  public hero: Hero;
+  public selectedHero: Hero;
+  public heroes: Hero[];
 
   constructor() {
-    this.hero = {
-      id: 1,
-      name: 'Windstorm'
-    };
+    this.heroes = HEROES;
   }
 
   ngOnInit(): void {
     
+  }
+
+  public onSelect(hero: Hero): void {
+    this.selectedHero = hero;
   }
 
 }

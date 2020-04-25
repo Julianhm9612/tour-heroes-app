30aa8cd40e4e857710e63ff416948ee35d27171b
julian Henao <julianhm9612@gmail.com>
2020-04-24 21:45:32 -0500

    Add new hero detail component

---
+++ src/app/hero-detail/hero-detail.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit, Input } from '@angular/core';
+
+// Interfaces
+import { Hero } from '../hero';
+
+@Component({
+  selector: 'app-hero-detail',
+  templateUrl: './hero-detail.component.html',
+  styleUrls: ['./hero-detail.component.css']
+})
+export class HeroDetailComponent implements OnInit {
+  @Input() hero: Hero;
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
